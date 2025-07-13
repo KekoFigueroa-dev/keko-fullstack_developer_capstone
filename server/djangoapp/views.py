8b62955e90f220497e8274c3e6314a5444cfdb31
KekoFigueroa-dev <thenaturalistgospel@gmail.com>
2025-07-13 04:48:56 -0400

    Add deployment files and update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -78,7 +78,8 @@ def get_cars(request):
     if count == 0:
         initiate()
     car_models = CarModel.objects.select_related('car_make')
-    cars = []     for car_model in car_models:
+    cars = []     
+    for car_model in car_models:
         cars.append({
             "CarModel": car_model.name,
             "CarMake": car_model.car_make.name
